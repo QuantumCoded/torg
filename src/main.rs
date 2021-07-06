07bb462fb0ed29e244ca1bc3f97752010c0651d0
Alex Bethel <abethel@cs.nmt.edu>
2021-07-06 17:47:00 -0500

    Basic tui stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use orgize::Org;
+
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Paragraph, Wrap};
+use tui::Terminal;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.clear()?;
+
+    let mut org_text_buf = std::fs::read_to_string("demo.org").unwrap();
+    let mut agenda_text_buf = String::new();
+
+    loop {
+        terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                .split(f.size());
+
+            let org_block = Block::default().title("org").borders(Borders::ALL);
+            let agenda_block = Block::default().title("agenda").borders(Borders::ALL);
+
+            let org_text = Paragraph::new(org_text_buf.as_ref()).wrap(Wrap { trim: false });
+
+            f.render_widget(org_text, org_block.inner(chunks[0]));
+
+            f.render_widget(org_block, chunks[0]);
+            f.render_widget(agenda_block, chunks[1]);
+        })?;
+
+        // gap buffer, or rope
+
+        std::thread::sleep(std::time::Duration::from_millis(25));
+    }
 }
