6e9f73b13a8c71bd89552e46e517ca26c1b7432d
Brendan Fields <mrbspony@gmail.com>
2021-07-06 18:22:02 -0500

    add tui widget

--- src/main.rs
+++ src/main.rs
@@ -18,20 +18,31 @@ fn main() -> Result<(), io::Error> {
 
     loop {
         terminal.draw(|f| {
-            let chunks = Layout::default()
+            let screen_chunks = Layout::default()
                 .direction(Direction::Horizontal)
                 .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
                 .split(f.size());
+            let left_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(70), Constraint::Percentage(30)])
+                .split(screen_chunks[0]);
+            let right_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(70), Constraint::Percentage(30)])
+                .split(screen_chunks[1]);
 
             let org_block = Block::default().title("org").borders(Borders::ALL);
             let agenda_block = Block::default().title("agenda").borders(Borders::ALL);
+            let file_block = Block::default().title("file").borders(Borders::ALL);
+            let calendar_block = Block::default().title("calendar").borders(Borders::ALL);
+            
+            // let org_text = Paragraph::new(org_text_buf.as_ref()).wrap(Wrap { trim: false });
+            // f.render_widget(org_text, org_block.inner(chunks[0]));
 
-            let org_text = Paragraph::new(org_text_buf.as_ref()).wrap(Wrap { trim: false });
-
-            f.render_widget(org_text, org_block.inner(chunks[0]));
-
-            f.render_widget(org_block, chunks[0]);
-            f.render_widget(agenda_block, chunks[1]);
+            f.render_widget(org_block, left_chunks[0]);
+            f.render_widget(agenda_block, right_chunks[0]);
+            f.render_widget(file_block, left_chunks[1]);
+            f.render_widget(calendar_block, right_chunks[1]);
         })?;
 
         // gap buffer, or rope
