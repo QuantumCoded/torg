2ddfa756d40a36504602ff5ca69f534dd1363586
Brendan Fields <mrbspony@gmail.com>
2021-07-06 19:41:48 -0500

    add file list

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use orgize::Org;
 use std::io;
 use tui::backend::CrosstermBackend;
 use tui::layout::{Constraint, Direction, Layout};
-use tui::widgets::{Block, Borders, Paragraph, Wrap};
+use tui::widgets::*;
 use tui::Terminal;
-
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
@@ -35,10 +34,31 @@ fn main() -> Result<(), io::Error> {
             let agenda_block = Block::default().title("agenda").borders(Borders::ALL);
             let file_block = Block::default().title("file").borders(Borders::ALL);
             let calendar_block = Block::default().title("calendar").borders(Borders::ALL);
-            
+
+            let file_list = List::new(
+                std::fs::read_dir(".")
+                    .unwrap()
+                    .filter_map(|dir| {
+                        if let Ok(entry) = dir {
+                            let path = entry.path();
+
+                            if path.is_file() {
+                                if let Some(name) = path.file_name() {
+                                    return Some(ListItem::new(name.to_str().unwrap().to_string()));
+                                }
+                            }
+                        }
+                        None
+                    })
+                    .collect::<Vec<_>>(),
+            );
+
             // let org_text = Paragraph::new(org_text_buf.as_ref()).wrap(Wrap { trim: false });
             // f.render_widget(org_text, org_block.inner(chunks[0]));
 
+            f.render_widget(file_list, file_block.inner(left_chunks[1]));
+
+            // render blocks
             f.render_widget(org_block, left_chunks[0]);
             f.render_widget(agenda_block, right_chunks[0]);
             f.render_widget(file_block, left_chunks[1]);
