5ed1d59f260bb715a3cae6c5fd9959b54e79351f
Alex Bethel <abethel@cs.nmt.edu>
2021-07-15 12:57:50 -0500

    Avoid reading directory every render frame

--- src/app.rs
+++ src/app.rs
@@ -89,6 +89,18 @@ impl<'a, B: Backend> App<'a, B> {
     }
 
     pub fn draw(&mut self) -> Result<CompletedFrame, std::io::Error> {
+        let file_list = List::new(
+            self.org_files
+                .iter()
+                .map(|file| ListItem::new(file.filename.to_string_lossy().to_string()))
+                .collect::<Vec<_>>(),
+        );
+
+        let org_block = Block::default().title("org").borders(Borders::ALL);
+        let agenda_block = Block::default().title("agenda").borders(Borders::ALL);
+        let file_block = Block::default().title("file").borders(Borders::ALL);
+        let calendar_block = Block::default().title("calendar").borders(Borders::ALL);
+
         self.term.draw(|f| {
             let screen_chunks = Layout::default()
                 .direction(Direction::Horizontal)
@@ -103,35 +115,10 @@ impl<'a, B: Backend> App<'a, B> {
                 .constraints([Constraint::Percentage(70), Constraint::Percentage(30)])
                 .split(screen_chunks[1]);
 
-            let org_block = Block::default().title("org").borders(Borders::ALL);
-            let agenda_block = Block::default().title("agenda").borders(Borders::ALL);
-            let file_block = Block::default().title("file").borders(Borders::ALL);
-            let calendar_block = Block::default().title("calendar").borders(Borders::ALL);
-
-            let file_list = List::new(
-                fs::read_dir(".")
-                    .unwrap()
-                    .filter_map(|dir| {
-                        if let Ok(entry) = dir {
-                            let path = entry.path();
-
-                            if path.is_file() {
-                                if let Some(name) = path.file_name() {
-                                    return Some(ListItem::new(name.to_str().unwrap().to_string()));
-                                }
-                            }
-                        }
-                        None
-                    })
-                    .collect::<Vec<_>>(),
-            );
-
-            // let org_text = Paragraph::new(org_text_buf.as_ref()).wrap(Wrap { trim: false });
-            // f.render_widget(org_text, org_block.inner(chunks[0]));
-
+            // Render block contents.
             f.render_widget(file_list, file_block.inner(left_chunks[1]));
 
-            // render blocks
+            // Render blocks.
             f.render_widget(org_block, left_chunks[0]);
             f.render_widget(agenda_block, right_chunks[0]);
             f.render_widget(file_block, left_chunks[1]);
